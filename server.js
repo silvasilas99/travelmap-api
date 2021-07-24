aa5e15514fd8a189bbfc77a14a2741db1edde9fb
Silas S. da Silva <silvasilas66@gmail.com>
2021-07-23 22:14:19 -0300

    Changing call of dotenv

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const app = express();
-const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const userRoute = require("./routes/users");
 const pinRoute = require("./routes/pins");
-
-dotenv.config();
+require('dotenv').config()
 
 mongoose
  	.connect(process.env.MONGO_URL, {
