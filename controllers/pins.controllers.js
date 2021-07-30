f118526f0d6398f693139d1f8db618bedf60b08a
Silas S. da Silva <silvasilas66@gmail.com>
2021-07-30 00:00:35 -0300

    Create and get Pins

--- controllers/pins.controllers.js
+++ controllers/pins.controllers.js
@@ -1,9 +1,28 @@
+const Pin = require("../models/Pin");
+
 module.exports = {
     async hello (req, res) {
         try {
             res.status(200).json ({ message: "Hello, user. Welcome to pins controllers!" });
         } catch (err) {
             res.status(500).json(err.message)
+        }
+    },
+    async createPin (req, res) {
+        const newPin = new Pin(req.body);
+        try {
+            const savedPin = await newPin.save();
+            res.status(200).json(savedPin);
+        } catch (err) {
+            res.status(500).json(err.message);
+        }
+    },
+    async getPins (req, res) {
+        try {
+            const pins = await Pin.find();
+            res.status(200).json(pins);
+        } catch (err) {
+            res.status(500).json(err.message);
         }
     },
 };
--- routes/pins.js
+++ routes/pins.js
@@ -2,5 +2,7 @@ const router = require('express').Router();
 const pinsController = require('./../controllers/pins.controllers');
 
 router.get('/', pinsController.hello);
+router.post('/new', pinsController.createPin);
+router.get('/allPins', pinsController.getPins);
 
 module.exports = router;
